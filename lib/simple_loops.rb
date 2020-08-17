8607d5883f7da75e92d8363ed7b445e628d44d58
Jake Chappelle <jake.m.chappelle@gmail.com>
2020-08-17 17:49:26 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -17,5 +17,11 @@ def output_array(array)
 end
 
 def return_string_array(array)
-  return array
+  count = 0
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s) # OR new_array << array[count].to_s
+    count += 1
+  end
+  new_array
 end
