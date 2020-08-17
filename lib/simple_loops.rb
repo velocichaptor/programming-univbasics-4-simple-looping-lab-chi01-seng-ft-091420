00d91c706bc1753f12d8985177eea030d640553b
Jake Chappelle <jake.m.chappelle@gmail.com>
2020-08-17 17:36:39 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,13 @@
 # Write your methods here
+
+def loop_message_five_times(string)
+end
+
+def loop_message_n_times(string, integer)
+end
+
+def output_array(array)
+end
+
+def return_string_array(array)
+end
