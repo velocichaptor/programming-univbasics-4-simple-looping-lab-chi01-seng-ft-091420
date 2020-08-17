a275975e15949e34623ad6e36645bbc448d3aede
Jake Chappelle <jake.m.chappelle@gmail.com>
2020-08-17 17:55:33 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,27 @@
 # Write your methods here
+
+def loop_message_five_times(string)
+  puts string
+  puts string
+  puts string
+  puts string
+  puts string
+end
+
+def loop_message_n_times(string, integer)
+  integer.times {puts string}
+end
+
+def output_array(array)
+  puts array
+end
+
+def return_string_array(array)
+  count = 0
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s)
+    count += 1
+  end
+  new_array
+end
